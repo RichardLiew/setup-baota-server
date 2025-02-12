66db5844cef12a098912a4043f2264b6e412d75c
Richard <RichardLiew@163.com>
2025-02-12 16:53:00 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+USERNAME='richard'
+PASSWORD='Anebit@2019'
+PORT='9527'
+SAFE_PATH=''
+IDC_CODE='ed8484bec'
+SSL_DISABLE='yes'
+
+ADMIN_PATH='/www/server/panel/data'
+ADMIN_AUTH='admin_path.pl'
+PANEL_URL='https://download.bt.cn/install/install_panel.sh'
+
+mkdir -p ${ADMIN_PATH}
+if [ -z "${SAFE_PATH}" ]; then
+    SAFE_PATH='/'
+fi
+echo "${SAFE_PATH}" > ${ADMIN_PATH}/${ADMIN_AUTH}
+
+curl -sSO ${PANEL_URL}
+
+bash $(basename ${PANEL_URL}) \
+    ${IDC_CODE} \
+    —user=${USERNAME} \
+    —password=${PASSWORD} \
+    —port=${PORT} \
+    --safe-path=${SAFE_PATH} \
+    -y \
+    $(if [ "${SSL_DISABLE}" = 'yes' ]; then echo '--ssl-disable'; fi)
